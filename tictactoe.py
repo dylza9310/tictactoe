6d8fbc303e79d3ac2408176c25c9ad537fef6571
Dylan Zago <dylan.zago82@gmail.com>
2025-08-07 16:39:04 +0100

    Fix game logic to allow both players to take turns and end the game correctly

--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,15 @@
 board = ['', '', '', '', '', '', '', '', '']
 
-while board.count("x") < 9:
+while board.count("x") + board.count("o") < 9:
     x = int(input("Player 1 pick a position (0-8): "))
-    if board[x] == '':
+    y = int(input("Player 2 pick a position (0-8): "))
+    if board[x] == '' or board[y] == '':
         board[x] = 'x'
+        board[y] = 'o'
         print(board)
+    if board.count("x") + board.count("o") >= 9:
+        print("Game Over. It's a draw!")
+        break
     else:
         print("Position already taken. Try again.")
 
