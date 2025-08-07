caff7df2af658844c1f48a0763e6d69952c74cc9
Dylan Zago <dylan.zago82@gmail.com>
2025-08-07 21:45:55 +0100

    Add intermediate computer mode and refactor game mode selection

--- tictactoe.py
+++ tictactoe.py
@@ -44,7 +44,7 @@ def human_vs_human():
                 restart_game()
 
 
-def human_vs_computer():
+def human_vs_easycomputer():
     import random
     while board.count("x") + board.count("o") < 9:
         x = int(input("Player pick a position (0-8): "))
@@ -78,14 +78,60 @@ def human_vs_computer():
             restart_game()
             return
 
+def human_vs_intermediatecomputer():
+    import random
+    win_conditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ]
+    while board.count("x") + board.count("o") < 9:
+        x = int(input("Player pick a position (0-8): "))
+        if board[x] == '':
+            board[x] = 'x'
+            print(board)
+            if check_win():
+                print("Player wins!")
+                restart_game()
+                return
+        else:
+            print("Position already taken. Try again.")
+            continue
+
+        print("Intermediate Computer's turn...")
+        # Placeholder for intermediate computer logic
+        blocked = False
+        for condition in win_conditions:
+            if all(board[i] == 'o' for i in condition if board[i] != '') and any(board[i] == '' for i in condition):
+                y = next(i for i in condition if board[i] == '')
+                board[y] = 'o'
+                blocked = True
+                print(f"Intermediate Computer chose position {y}")
+                break
+        if not blocked:
+            y = random.choice([i for i, v in enumerate(board) if v == ''])
+            board[y] = 'o'
+            print(f"Intermediate Computer chose position {y}")
+            print(board)
+        if check_win():
+            print("Intermediate Computer wins!")
+            restart_game()
+            return
+
+        if board.count("x") + board.count("o") >= 9:
+            print("Game Over. It's a draw!")
+            restart_game()
+            return
 
 str(print("Welcome to Tic Tac Toe!"))
-str(print("Choose game mode: 1 for Human vs Human, 2 for Human vs Computer"))
+str(print("Choose game mode: 1 for Human vs Human, 2 for Human vs Easy Computer, 3 for Human vs Intermediate Computer"))
 game_mode = input("Enter 1 or 2: ")
 if game_mode == '1':
     human_vs_human()
 elif game_mode == '2':
-    human_vs_computer()
+    human_vs_easycomputer()
+elif game_mode == '3':
+    human_vs_intermediatecomputer()
 else:
     print("Invalid choice. Please restart the game and choose a valid mode.")
     exit()
