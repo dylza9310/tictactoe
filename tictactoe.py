4ea447f17295703eb0d7bd83c24bd147039ef35a
Dylan Zago <dylan.zago82@gmail.com>
2025-08-07 16:47:37 +0100

    Refactor player input handling to ensure correct turn-taking and position validation

--- tictactoe.py
+++ tictactoe.py
@@ -2,15 +2,22 @@ board = ['', '', '', '', '', '', '', '', '']
 
 while board.count("x") + board.count("o") < 9:
     x = int(input("Player 1 pick a position (0-8): "))
-    y = int(input("Player 2 pick a position (0-8): "))
     if board[x] == '' or board[y] == '':
         board[x] = 'x'
+        print(board)
+    else:
+        print("Position already taken. Try again.")
+
+    y = int(input("Player 2 pick a position (0-8): "))
+    if board[x] == '' or board[y] == '':
         board[y] = 'o'
         print(board)
+    else:
+        print("Position already taken. Try again.")
+
     if board.count("x") + board.count("o") >= 9:
         print("Game Over. It's a draw!")
         break
-    else:
-        print("Position already taken. Try again.")
+    
 
 print(board)
