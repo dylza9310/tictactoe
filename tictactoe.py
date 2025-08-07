14a166f65745748a0b7be7dff0410763f33e5420
Dylan Zago <dylan.zago82@gmail.com>
2025-08-07 17:20:32 +0100

    Add win condition check for both players in Tic Tac Toe game

--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,21 @@
 board = ['', '', '', '', '', '', '', '', '']
 
+def check_win():
+    if board[x] or board[y]:
+        win_conditions = [0, 1, 2], [3, 4, 5], [6, 7, 8], \
+                        [0, 3, 6], [1, 4, 7], [2, 5, 8], \
+                        [0, 4, 8], [2, 4, 6]
+    return True if any(all(board[i] == 'x' for i in condition) for condition in win_conditions) or \
+                   any(all(board[i] == 'o' for i in condition) for condition in win_conditions) else False
+    
 while board.count("x") + board.count("o") < 9:
     x = int(input("Player 1 pick a position (0-8): "))
     if board[x] == '' or board[y] == '':
         board[x] = 'x'
         print(board)
+        if check_win():
+            print("Player 1 wins!")
+            break
     else:
         print("Position already taken. Try again.")
 
@@ -12,12 +23,15 @@ while board.count("x") + board.count("o") < 9:
     if board[x] == '' or board[y] == '':
         board[y] = 'o'
         print(board)
+        if check_win():
+            print("Player 2 wins!")
+            break
     else:
         print("Position already taken. Try again.")
 
-    if board.count("x") + board.count("o") >= 9:
+    if board.count("x") + board.count("o") >= 8:
         print("Game Over. It's a draw!")
         break
-    
+
 
 print(board)
